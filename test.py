63103162126ab678f9d732cd9185bd07a0226487
Shanen2003 <shanenleebateman@gmail.com>
2025-12-01 18:12:41 -0500

    Tuned Inputted

--- test.py
+++ test.py
@@ -246,8 +246,8 @@ dtest  = xgb.DMatrix(data=X_test.values,  label=y_test)
 # # Count values
 counts = pd.Series(y_train).value_counts().sort_index()
 neg = int(counts.get(0, 0)); pos = int(counts.get(1, 0)) # Calculate positive and negative samples
-# print(f"Number of negative samples: {neg}")
-# print(f"Number of positive samples: {pos}")
+print(f"Number of negative samples: {neg}")
+print(f"Number of positive samples: {pos}")
 
 # Calculate ratio
 ratio = neg / pos
@@ -257,29 +257,29 @@ w_tr = np.where(y_train == 1, ratio, 1.0).astype(np.float32)
 # Build weighted DMatrices
 dtrain_w = xgb.DMatrix(X_train.values, label=y_train, weight=w_tr)
 
-# watchlist = [(dtrain_w, "train")] # Set data for evaluation
-# xgb_w = xgb.train(params, # Set parameters
-#                     dtrain_w,  # Set training data
-#                     num_boost_round=3, # Set number of rounds
-#                     evals=watchlist,  # Set data to evaluate on
-#                     verbose_eval=50) # Set print out frequency
+watchlist = [(dtrain_w, "train")] # Set data for evaluation
+xgb_w = xgb.train(params, # Set parameters
+                    dtrain_w,  # Set training data
+                    num_boost_round=3, # Set number of rounds
+                    evals=watchlist,  # Set data to evaluate on
+                    verbose_eval=50) # Set print out frequency
 
-# test_pred_w = xgb_w.predict(dtest) # Create predictions
-
-
-# # Convert predictions into classes at 0.5
-# test_pred_cls_w = (test_pred_w >= 0.5).astype(int)
+test_pred_w = xgb_w.predict(dtest) # Create predictions
 
 
-# print("\nConfusion matrix:")
-# cm = (confusion_matrix(y_test, test_pred_cls_w))
-# disp = ConfusionMatrixDisplay(confusion_matrix=cm)# Set class labels
-# disp.plot(cmap="Blues") # Set color map
-# plt.title("Confusion Matrix — Weighted XGBoost") # Set title
-# plt.savefig("confusion_matrix_weighted.png", dpi=300, bbox_inches="tight")
-# plt.close()
-# print("\nAccuracy):")
-# print(accuracy_score(y_test, test_pred_cls_w)) # Get Accuracy
+# Convert predictions into classes at 0.5
+test_pred_cls_w = (test_pred_w >= 0.5).astype(int)
+
+
+print("\nConfusion matrix:")
+cm = (confusion_matrix(y_test, test_pred_cls_w))
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)# Set class labels
+disp.plot(cmap="Blues") # Set color map
+plt.title("Confusion Matrix — Weighted XGBoost") # Set title
+plt.savefig("confusion_matrix_weighted.png", dpi=300, bbox_inches="tight")
+plt.close()
+print("\nAccuracy):")
+print(accuracy_score(y_test, test_pred_cls_w)) # Get Accuracy
 
 
 
@@ -636,8 +636,8 @@ dtrain_w = xgb.DMatrix(X_train.values, label=y_train, weight=w_tr)
 
 
 
-# Best Parameters 
-# DO NOT RUN THE TUNING PARAMETERS IT TAKES AROUND 90 MINUTES ON CRC
+# Best Parameters & Weighted
+# DO NOT RUN THE TUNING PARAMETERS IT TAKES AROUND 1 HOUR ON CRC
 
 max_depth        = 10
 min_child_weight = 5
@@ -646,3 +646,64 @@ subsample        = 0.6
 colsample_bytree = 0.6
 eta              = 0.3
 best_round       = 5
+
+
+
+
+
+
+
+
+# Count values
+counts = pd.Series(y_train).value_counts().sort_index()
+neg = int(counts.get(0, 0)); pos = int(counts.get(1, 0)) # Calculate positive and negative samples
+print(f"Number of negative samples: {neg}")
+print(f"Number of positive samples: {pos}")
+
+# Calculate ratio
+ratio = neg / pos
+# Set ratio as weight for positive samples
+w_tr = np.where(y_train == 1, ratio, 1.0).astype(np.float32)
+
+# Build weighted DMatrices
+dtrain_w = xgb.DMatrix(X_train.values, label=y_train, weight=w_tr)
+
+params = {
+    "objective": "binary:logistic",
+    "eval_metric": ["auc", "error"],
+    "max_depth": max_depth, # Use tuned value for max depth
+    "min_child_weight": min_child_weight, # Use tuned value for min_child_weight
+    "gamma": gamma, # Use tuned value for gamma
+    "subsample": subsample, # Use tuned value for subsample
+    "colsample_bytree": colsample_bytree, # Use tuned value for colsample_bytree
+    "eta": eta, # Use tuned value for eta
+    "tree_method": "hist",
+    "seed": 111111,
+    "nthread": 1,                  # single core
+}
+
+num_boost_round = best_round # Set number of rounds
+
+watchlist = [(dtrain_w, "train")] # Set data for evaluation
+xgb_w = xgb.train(params, # Set parameters
+                    dtrain_w,  # Set training data
+                    num_boost_round=num_boost_round, # Set number of rounds
+                    evals=watchlist,  # Set data to evaluate on
+                    verbose_eval=50) # Set print out frequency
+
+test_pred_w = xgb_w.predict(dtest) # Create predictions
+
+
+# Convert predictions into classes at 0.5
+test_pred_cls_w = (test_pred_w >= 0.5).astype(int)
+
+
+print("\nConfusion matrix:")
+cm = (confusion_matrix(y_test, test_pred_cls_w))
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)# Set class labels
+disp.plot(cmap="Blues") # Set color map
+plt.title("Confusion Matrix — Weighted XGBoost") # Set title
+plt.savefig("confusion_matrix_weighted_and_tuned.png", dpi=300, bbox_inches="tight")
+plt.close()
+print("\nAccuracy):")
+print(accuracy_score(y_test, test_pred_cls_w)) # Get Accuracy
